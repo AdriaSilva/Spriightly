6ff3e777c40735f67bc57ff4e8a4a856d396eee4
Adriana <100482041+AdriaSilva@users.noreply.github.com>
2023-09-17 08:37:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var num = 15 * 60;
+
+
+
+function startTime(){
+    const element = document.getElementById("demo");
+    state = 'counting';
+    var x = setInterval(function() {
+        num -= 1;
+        var minutes = Math.floor(num/60);
+        var seconds = num % 60;
+    
+        element.innerHTML = minutes + ":" + seconds;
+        if (num < 0 || state == 'stopped'){
+            stopTime(x);
+        }
+    }, 1000);
+};
+
+function stopTime(x){
+    state = 'stopped';
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = minutes + ":" + seconds ;
+};
+
+function breakTime(){
+    var checkBox = document.getElementById("mySlider");
+    if (checkBox.checked == true){
+        num = 5 * 60
+    }
+    else{
+        num = 15 * 60
+    }
+}
